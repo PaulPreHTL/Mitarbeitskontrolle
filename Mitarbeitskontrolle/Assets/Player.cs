5988b9396d630e353eba6e034a9203d79e93e6e0
RetroRitter69 <114225651+RetroRitter69@users.noreply.github.com>
2023-01-27 12:13:39 +0100

    Fertig

--- Mitarbeitskontrolle/Assets/Player.cs
+++ Mitarbeitskontrolle/Assets/Player.cs
@@ -12,6 +12,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //GetKeyDown passiert nur einmal
+        //Deshalb GetKey
+        
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            Debug.Log("Shift gedrückt");
+            transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * 2, 0, Input.GetAxis("Vertical") * speed * 2) * Time.deltaTime;
+            //transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * 10, 0, Input.GetAxis("Vertical") * speed * 10) * Time.deltaTime;
+            return;
+        }
         transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed) * Time.deltaTime;
     }
 }
